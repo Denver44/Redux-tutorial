4e97076cfc9daeba958c8d90102fdc1f1f612cb9
durgeshlinearloop <durgesh@linearloop.io>
2021-06-20 12:53:13 +0530

    memoziation using loadsh

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
     - Created a post list and user list .
     - here we have used redux thunk to make async call.
     - as to make async call we need a middleware so here we used thunk.
+    - Here we have used lodash to memoize our network call.
+    
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,14 @@
 import jsonPlaceHolder from "../api/jsonPlaceHolder";
+import _ from "lodash";
 
 export const fetchPost = () => async (dispatch) => {
   const response = await jsonPlaceHolder.get("/posts");
   dispatch({ type: "FETCH_POST", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
-  const response = await jsonPlaceHolder.get(`/users/${id}`);
-  dispatch({ type: "FETCH_USER", payload: response.data });
-};
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const { data } = await jsonPlaceHolder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: data });
+});
