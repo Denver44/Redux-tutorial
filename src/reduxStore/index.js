40ee49b0847181ef9c72a2b37e69edad25558a66
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-13 01:34:50 +0530

    minor fix

--- src/reduxStore/index.js
+++ src/reduxStore/index.js
@@ -2,6 +2,6 @@ import { combineReducers } from "redux";
 import { postReducer, userReducer } from "../reducers";
 
 export default combineReducers({
-  post: postReducer,
+  posts: postReducer,
   user: userReducer,
 });
